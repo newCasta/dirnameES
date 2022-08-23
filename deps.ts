b07edde4a6db287eb3847065fae82c02aa544a42
siCasta <sicastaprojectos@gmail.com>
2022-08-23 08:20:04 -0500

    change funtion filename

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
-export { dirname, normalize } from 'https://deno.land/std@0.151.0/path/mod.ts'
+export { dirname } from 'https://deno.land/std@0.151.0/path/mod.ts'
+export { fileURLToPath } from 'https://deno.land/std@0.127.0/node/url.ts'
+
--- src/dirname.ts
+++ src/dirname.ts
@@ -1,9 +1,23 @@
-import { normalize, dirname as dir } from '../deps.ts'
+import { dirname as dir, fileURLToPath } from '../deps.ts'
 
+/**
+ * Gets the filename of a path.
+ * @param meta The import meta object.
+ * ```typescript
+ * const __filename = filename(import.meta)
+ * ```
+*/
 export function filename(meta: ImportMeta): string {
-    return normalize(meta.url).replace('file:\\', '')
+    return fileURLToPath(meta.url)
 }
 
+/**
+ * Gets the directory name of a path.
+ * @param meta The import meta object.
+ * ```typescript
+ * const __dirname = dirname(import.meta)
+ * ```
+*/
 export function dirname(meta: ImportMeta): string {
     return dir(filename(meta))
 }
